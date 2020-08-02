c62cf1d1f4c979b326840c5b2be88b887faf0ac0
xodnd13 <63281272+xodnd13@users.noreply.github.com>
2020-08-02 23:53:10 +0900

    Update solution.py

--- 곽태웅/쇠막대기/solution.py
+++ 곽태웅/쇠막대기/solution.py
@@ -1,21 +1,22 @@
 def solution(inp):
-    cnt_list = []
-    answer = 0
+    cnt_st, answer = 0, 0
     top = ''
     for i in inp:
         if i == '(':
-            cnt_list.append(1)
+            cnt_st += 1
+            answer += 1
         elif i == ')':
             if top == '(':
-                del cnt_list[-1]
-                cnt_list = [cnt_list + 1 for cnt_list in cnt_list]
+                cnt_st -= 1
+                answer -= 1
+                answer += cnt_st
             else:
-                answer += cnt_list[-1]
-                del cnt_list[-1]
+                cnt_st -= 1
         top = i
         
     return answer
 
 print(solution(input()))
 
-## 0802, 23:23 수정
+## 수정 0802, 23:23
+## 수정 0802, 23:48
