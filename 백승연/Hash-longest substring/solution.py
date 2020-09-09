bde53f7ff9a02e943d56adc552b70d492bb69af9
kyliebaek <chris8w8@naver.com>
2020-09-09 15:03:53 +0900

    better solution

--- 백승연/Hash-longest substring/solution.py
+++ 백승연/Hash-longest substring/solution.py
@@ -1,5 +1,23 @@
-s1 = "aasdfee"
+#모범답안
 
 
-for i, s in enumerate(s1):
-    print(i,s)
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        arr = {}
+        start = 0
+        length = 0
+
+        for index, char in enumerate(s):
+            if char in arr and start <= arr[char]:
+                start = arr[char] + 1
+            else:
+                length = max(length, index - start + 1)
+
+            arr[char] = index
+
+        return length
+
+
+s1 = Solution()
+
+print(s1.lengthOfLongestSubstring('abcdae'))
