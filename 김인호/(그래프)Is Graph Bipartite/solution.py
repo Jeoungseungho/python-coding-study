c29795a7790f5d2c23cd7af29c342fad6c8cc498
kih1024 <rladlsgh654@naver.com>
2020-09-21 00:44:19 +0900

    Modify bipartite

--- 김인호/(그래프)Is Graph Bipartite/solution.py
+++ 김인호/(그래프)Is Graph Bipartite/solution.py
@@ -1,14 +1,13 @@
 # bipartite 그래프 : 다른 그룹의 정점이 간선으로 연결되어져 있는(<=> 같은 그룹에 속한 정점끼리는 서로 인접하지 않도록 하는) 그래프
 from collections import deque
 
-
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         visited = [0] * len(graph)
 
         for v in range(len(graph)):
             if not visited[v]:
-                #               아래부턴 BFS 코드
+                # 아래부턴 BFS 코드
                 dq = deque([v])
                 visited[v] = 1
 
