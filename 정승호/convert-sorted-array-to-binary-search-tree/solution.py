16a60ca794533586ab16f5722f427be143d84e8f
Jeoungseungho <platoon07@khu.ac.kr>
2020-12-25 23:06:45 +0900

    분할정복으로 해결한 LeetCode convert-sorted-array-to-binary-search-tree입니다.

---
+++ 정승호/convert-sorted-array-to-binary-search-tree/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        
+        # 분할 정복으로 이진 검색 결과 트리 구성 
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+        
+        return node
