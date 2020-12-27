7374324ad7ab01160244959c1442726e4db4c9c5
Jeoungseungho <platoon07@khu.ac.kr>
2020-12-27 21:03:18 +0900

    DFS 가지치기로 해결한 LeetCode range sum of bst입니다.

---
+++ 정승호/range-sum-of-bst/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        def dfs(node: TreeNode):
+            if not node:
+                return 0
+            
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+        
+        return dfs(root)
