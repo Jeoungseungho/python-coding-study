0c2c47c8a60c599c4ac0337b78456a3026c07c30
Jeoungseungho <platoon07@khu.ac.kr>
2021-01-02 23:25:43 +0900

    Dynamic Programming으로 해결한 LeetCode regular expression matching입니다.

---
+++ 정승호/regular-expression-matching/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        s, p = ' '+ s, ' '+ p
+        lenS, lenP = len(s), len(p)
+        dp = [[0]*(lenP) for i in range(lenS)]
+        dp[0][0] = 1
+
+        for j in range(1, lenP):
+            if p[j] == '*':
+                dp[0][j] = dp[0][j-2]
+
+        for i in range(1, lenS):
+            for j in range(1, lenP):
+                if p[j] in {s[i], '.'}:
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j] == "*":
+                    dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
+
+        return bool(dp[-1][-1])
+        
+
+
+
+
